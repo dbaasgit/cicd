999bca0246a64f5278019dcbd43348b1f65540aa
Anurag <75475233+dbaasgit@users.noreply.github.com>
2021-04-08 18:20:20 +0100

    Update cicdsubnet.tf

--- cicdsubnet.tf
+++ cicdsubnet.tf
@@ -1,12 +1,7 @@
 provider "aws" {
-  alias   = "eks_master"
   region  = "us-east-2"
 
-  assume_role = [{
-    role_arn = "arn:aws:iam::097184783241:role/ec2-IAMRole"
-  }]
-}
-resource "aws_subnet" "newcicd" {
+ resource "aws_subnet" "newcicd" {
   vpc_id     = "vpc-d1c99cb9"
   cidr_block = "172.31.64.0/24"
 
